25d7b11808c666ad31ee455f369ac3206f145df6
GitAndrewCoffee <andrew.mcneal.us@gmail.com>
2022-01-06 21:25:27 -0600

    Initial file draft

---
+++ index.js
@@ -0,0 +1,64 @@
+//
+// Global Variables & Constants
+//
+
+//NPM Packages
+
+const inquirer = require('inquirer');
+const mysql = require('mysql12');
+const cTable = require('console.table');
+
+//
+// Database Connection
+//
+
+// Write connection to DB
+
+//
+// Prompts
+//
+
+// Main Prompt
+    //View All Departments
+    //View all roles
+    //View all employees
+    //Add a department
+    //Add a role
+    //add an employee
+    //Update an employee role
+
+//Display Departments
+    //Load Departments from DB
+    //Format & Display
+    //Return to Prompt    
+
+//Display Roles
+    //Load Roles 
+    //Format and Display
+    //Return to Prompt
+
+//Display Employees
+    //Load Employees
+    //Format and Display
+    //Return to Prompt
+
+//Add Department
+    //Prompt for Department Info
+    //Save to Database
+    //Return to Prompt
+
+//Add Role
+    //Prompt for Role Info
+    //Save to Database
+    //Return to Prompt
+
+//Add Employee
+    //Prompt for Employee Info
+    //Save to Database
+    //Return to Prompt
+
+//Update Employee
+    //Prompt Employee Info
+    //Save to Database
+    //Return to Prompt
+
